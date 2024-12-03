7d01d07b925566722b79e89ad76c7137ad2369ab
Vidal <darkvidal@gmail.com>
2024-12-02 22:19:52 -0600

    feature: adding css styles for hero section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
+import RestauratFood from '../assets/images/restauranfood.jpg'
+
 const Main = () => {
     return (
         <main>
-            Main Section
+            <section className="hero">
+                <article >
+                    <h2 className="display-title">Little Lemon</h2>
+                    <h3 className="display-sub-title">Chicago</h3>
+                    <p className="description-item">
+                        We are family owned Mediterranean restaurant, 
+                        focused in traditional recipes served with a modern twist.
+                    </p>
+                    <button>Reserve a Table</button>
+                </article>
+                <article >
+                    <img src={RestauratFood} alt="" className="restaurant-food"/>
+                </article>
+            </section>
         </main>
     )
 }
