845c9b8bb76d85dc519c38e740ef6042eda41a22
Vidal <darkvidal@gmail.com>
2024-12-03 22:54:18 -0600

    feature: adding styles for order delivery and props attributes

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,9 +15,9 @@ const Main = () => {
             <button className="yellow-button">Online Menu</button>
           </div>
           <div className="specials-content">
-            <SpecialFoodItem itemPhoto={GreekSalad} />
-            <SpecialFoodItem itemPhoto={Pasta} />
-            <SpecialFoodItem itemPhoto={Taps} />
+            <SpecialFoodItem itemPhoto={GreekSalad} price="12.99" title="Greek Salad" description="The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons."/>
+            <SpecialFoodItem itemPhoto={Pasta} price="5.00" title="Pasta" description="This comes straight from grandma's recipe book, every last ingredient hass been sourced and is as authenthic as can be imagined."/>
+            <SpecialFoodItem itemPhoto={Taps} price="5.99" title="Bruchetta" description="Our Bruscheta is made from grilled bread that has been smeared with garlic and seasoned with alt and olive oil."/>
           </div>
         </div>
       </section>
--- src/components/SpecialFoodItem.js
+++ src/components/SpecialFoodItem.js
@@ -1,18 +1,22 @@
+import BycicleIcon from "../assets/images/bycicle-icon.jpg"
+
 const SpecialFoodItem = (props) => {
-    return (<div className="specials-item">
-        <img src={props.itemPhoto} alt="" className="specials-item-img" />
-        <div className="specials-item-details">
-          <div className="specials-item-title-price">
-            <h3>Title</h3>
-            <p>$12.99</p>
-          </div>
-          <p>
-            The famous greek salad of crispy lettuce, peppers, olives and
-            our Chicago style feta cheese, garnished with crunchy garlic
-            and rosemary croutons.
-          </p>
+  return (
+    <div className="specials-item">
+      <img src={props.itemPhoto} alt="" className="specials-item-img" />
+      <div className="specials-item-details">
+        <div className="specials-item-title-price">
+          <h3>{props.title}</h3>
+          <p>${props.price}</p>
         </div>
-      </div>)
+        <p>{props.description}</p>
+        <div className="order-delivery">
+          <a href="#">Order a delivery</a>
+          <img src={BycicleIcon} alt="" className="bycicle-icon" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SpecialFoodItem
