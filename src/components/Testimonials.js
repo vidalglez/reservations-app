4399a47df859a821d15229a8deaa79a32e96b8e7
Vidal <darkvidal@gmail.com>
2024-12-04 22:54:22 -0600

    feature: including missing key for testimonial item component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,24 +4,28 @@ const Testimonials = () => {
 
     const testimonials = [
         {
+            id: 1,
             name: 'Max Kenneth',
             rating: 5,
             opinion: 'Really enjoyed the atmosphere.',
             imagePath: 'person1.jpg'
         },
         {
+            id: 2,
             name: 'Kelly Smith',
             rating: 3,
             opinion: 'Place is good, but there was an issue while booking table',
             imagePath: 'person2.jpg'
         },
         {
+            id: 3,
             name: 'Brandon Clagget',
             rating: 4,
             opinion: 'You have to try the Greek Salad!',
             imagePath: 'person3.jpg'
         },
         {
+            id:4,
             name: 'Robert Rodriguez',
             rating: 5,
             opinion: 'Awesome place peaceful atmosphere with delicious food',
@@ -35,7 +39,7 @@ const Testimonials = () => {
                 <h1 >Testimonials</h1>
                 <div className="testimonials-content">
                     {
-                        testimonials.map(testimonialItem => <TestimonialItem testimonial={testimonialItem}/>)
+                        testimonials.map(testimonialItem => <TestimonialItem testimonial={testimonialItem} key={testimonialItem.id}/>)
                     }
                 </div>
             </div>
