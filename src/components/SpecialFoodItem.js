7b4b86b47ae627bc0748c8ddd08454c42cc48b30
Vidal <darkvidal@gmail.com>
2024-12-17 21:30:46 -0600

    refactor: replacing props with object destructuring for simplicity

--- src/components/SpecialFoodItem.js
+++ src/components/SpecialFoodItem.js
@@ -1,15 +1,15 @@
 import BycicleIcon from "../assets/images/bycicle-icon.jpg"
 
-const SpecialFoodItem = (props) => {
+const SpecialFoodItem = ({special}) => {
   return (
     <div className="specials-item">
-      <img src={require(`../assets/images/${props.special.image}`)} alt="" className="specials-item-img" />
+      <img src={require(`../assets/images/${special.image}`)} alt="" className="specials-item-img" />
       <div className="specials-item-details">
         <div className="specials-item-title-price">
-          <h3>{props.special.title}</h3>
-          <p>${props.special.price}</p>
+          <h3>{special.title}</h3>
+          <p>${special.price}</p>
         </div>
-        <p>{props.special.description}</p>
+        <p>{special.description}</p>
         <div className="order-delivery">
           <a href="#">Order a delivery</a>
           <img src={BycicleIcon} alt="" className="bycicle-icon" />
--- src/components/TestimonialItem.js
+++ src/components/TestimonialItem.js
@@ -1,19 +1,19 @@
 import Star from "../assets/images/star.png"
 
-const TestimonialItem = (props) => {
+const TestimonialItem = ({ testimonial }) => {
   return (
     <div className="testimonial-item">
       <div className="ratings">
-        {Array.from({ length: props.testimonial.rating }).map((_, index) => (
+        {Array.from({ length: testimonial.rating }).map((_, index) => (
           <img key={index} src={Star} alt="star" className="star-icon" />
         ))}
       </div>
       <div className="testimonial-details">
-      <img src={require(`../assets/images/${props.testimonial.imagePath}`)} alt="person" className="testimonial-image-person" />
-        <p>{props.testimonial.name}</p>
+      <img src={require(`../assets/images/${testimonial.imagePath}`)} alt="person" className="testimonial-image-person" />
+        <p>{testimonial.name}</p>
       </div>
       <div className="testimonial-review">
-        <p>"{props.testimonial.opinion}"</p>
+        <p>"{testimonial.opinion}"</p>
       </div>
     </div>
   )
