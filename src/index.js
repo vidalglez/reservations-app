eff31f67aaa132b12c6339a4b5c673581e12b39e
Vidal <darkvidal@gmail.com>
2025-01-14 17:46:57 -0600

    fix: unit test failing

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/tests/reducers/AvailableTimesReducer.test.js
+++ src/tests/reducers/AvailableTimesReducer.test.js
@@ -4,26 +4,18 @@ import {
   fetchAPI
 } from "../../reducers/AvailableTimesReducer"
 
-// jest.mock("../../reducers/AvailableTimesReducer", () => ({
-//   ...jest.requireActual("../../reducers/AvailableTimesReducer"),
-//   fetchAPI: jest.fn()
-// }))
-
 describe("Validate reducers", () => {
 
   beforeEach(() => {
     jest.resetModules()
-    //fetchAPI.mockClear()
-    //fetchAPI.mockReturnValue(["17:00", "17:30", "18:00", "19:00", "19:30", "20:00", "21:30", "22:30", "23:00"])
     jest.mock("../../reducers/AvailableTimesReducer", () => ({
-      //...jest.requireActual("../../reducers/AvailableTimesReducer"),
       fetchAPI: jest.fn().mockReturnValue(["17:00", "17:30", "18:00", "19:00", "19:30", "20:00", "21:30", "22:30", "23:00"])
     }))
   })
 
   test("Retrives same values provided in state", () => {
     const expectedState = {
-      availableTimes: ["17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "23:30"],
+      availableTimes: fetchAPI(new Date()),
       selectedDate: "",
     }
     expect(updateTimes(expectedState, {})).toEqual(initializeTimes)
